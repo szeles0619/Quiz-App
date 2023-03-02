6f9cc5ac3fbc7a992f7e6a8827c4f4e880c06254
Istvan <szeles0619@gmail.com>
2023-03-02 14:17:27 +0000

    Added sounds effect

---
+++ assets/js/logic.js
@@ -0,0 +1,6 @@
+//Sound effect on click when user answers
+let soundOn = true;
+const correctSound = new Audio('./assets/sfx/correct.wav');
+const incorrectSound = new Audio('./assets/sfx/incorrect.wav');
+
+
---
+++ assets/js/questions.js
@@ -0,0 +1,59 @@
+//Questions selected from https://opentdb.com/api.php?amount=10&category=28&difficulty=medium
+const questions = [
+    {
+        question: "Which Japanese company is the world&#039;s largest manufacturer of motorcycles?",
+        answer: [
+            "Yamaha",
+            "Honda",
+            "Suzuki",
+            "Kawasaki"],
+        correct_answer: "Honda",
+    },
+    {
+        question: "Complete the following analogy: Audi is to Volkswagen as Infiniti is to ?",
+        answer: [
+            "Honda",
+            "Hyundai",
+            "Subaru",
+            "Nissan"
+        ],
+        correct_answer: "Nissan"
+
+    },
+    {
+        question: "Which supercar company is from Sweden?",
+        answer: [
+            "Bugatti",
+            "Koenigsegg",
+            "Lamborghini",
+            "McLaren"
+        ],
+        correct_answer: "Koenigsegg",
+    },
+    {
+        question: "Ferrari has never made a V10 engine for any of its cars.",
+        answer: [
+            "True",
+            "False"
+        ],
+        correct_answer: "False"
+    },
+    {
+        question: "When was Cadillac founded?",
+        answer: [
+            "1964",
+            "1898",
+            "1902",
+            "1985"
+        ],
+        correct_answer: "1902"
+    },
+    {
+        question: "The majority of Subaru vehicles are made in China.",
+        answer: [
+            "True",
+            "False"
+        ],
+        correct_answer: "False"
+    }
+]
