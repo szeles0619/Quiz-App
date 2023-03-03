7bcb7599ef63f01ed062f512300ded0a7670ce52
Istvan <szeles0619@gmail.com>
2023-03-03 14:09:35 +0000

    Added function to display scores

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,2 +1,27 @@
+let clearBtn = document.getElementById("clear");
 
+function displayScores() {
+  let savedHighscores = JSON.parse(
+    window.localStorage.getItem("savedHighscores")
+  ); 
+  savedHighscores.sort(function (a, b) {
+    return b.score - a.score;
+  });
+
+  savedHighscores.forEach(function (score) {
+    let scoreLi = document.createElement("li");
+    scoreLi.textContent = score.initials + " - " + score.score;
+    let scoreOl = document.getElementById("highscores");
+    scoreOl.appendChild(scoreLi);
+  });
+}
+
+
+function clearScores() {
+  window.localStorage.removeItem("savedHighscores");
+  window.location.reload();
+}
+
+clearBtn.addEventListener("click", clearScores);
+displayScores();
 
