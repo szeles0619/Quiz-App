0ba1e694193d533ef980a69a757802e763c64346
Istvan <szeles0619@gmail.com>
2023-03-03 14:15:56 +0000

    Added final touches before submission to score.js file

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,5 +1,6 @@
 let clearBtn = document.getElementById("clear");
 
+//This functions is displays the scores from localStorage
 function displayScores() {
   let savedHighscores = JSON.parse(
     window.localStorage.getItem("savedHighscores")
@@ -8,20 +9,25 @@ function displayScores() {
     return b.score - a.score;
   });
 
+  //New list item
   savedHighscores.forEach(function (score) {
     let scoreLi = document.createElement("li");
     scoreLi.textContent = score.initials + " - " + score.score;
+
     let scoreOl = document.getElementById("highscores");
+    //Items will be added to ordered list
     scoreOl.appendChild(scoreLi);
   });
 }
 
-
+//This function is remove items from local storage
 function clearScores() {
   window.localStorage.removeItem("savedHighscores");
   window.location.reload();
 }
 
 clearBtn.addEventListener("click", clearScores);
+
 displayScores();
+
 
